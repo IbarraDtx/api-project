975596414eea8640f4f437903d2f04b8648c113b
Angel Ibarra <ibarragc.iska@gmail.com>
2024-07-09 18:45:07 -0600

    Corrección de rutas en autenticación de usuarios

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-const authMiddleware = require('../middlewares/authMiddleware');
-const roleMiddleware = require('../middlewares/roleMiddleware');
+const authMiddleware = require('../middleware/authMiddleware');
+const roleMiddleware = require('../middleware/roleMiddleware');
 
 router.post('/register', authMiddleware, roleMiddleware('admin'), authController.registrar);
 router.post('/login', authController.login);
